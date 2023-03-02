2ffbc540f011c4cba63ab39bf8e082bf85a6b035
Jenni <jenni@ohsomodern.org>
2023-03-02 11:45:36 +0200

    example

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [isFed, setIsFed] = useState(false);
+  const [fedTime, setFedTime] = useState<Date | null>(null);
+
+  useEffect(() => {
+    // Check on load of component, if animal is fed
+    const storedTime = localStorage.getItem('animal-1-feed-time');
+    if (storedTime === null) {
+      // Animal has never been fed
+    } else {
+      checkIfNeedsFood(storedTime);
+    }
+  }, []);
+
+  function checkIfNeedsFood(lastFed: string) {
+    const lastFedDate = new Date(lastFed);
+    const rightNow = new Date();
+
+    // TODO - Handle if day has changed too
+    // tip: https://bfy.tw/TsVk
+    if (lastFedDate.getHours() + 3 < rightNow.getHours()) {
+      // Time to feed again
+      setIsFed(false);
+    } else {
+      setIsFed(true);
+    }
+  }
+
+  function feedAnimal() {
+    setIsFed(true);
+    localStorage.setItem('animal-1-feed-time', new Date().toString());
+  }
+
+  return (
+    <div className="App">
+      <button onClick={feedAnimal} disabled={isFed}>Mata</button>
+    </div>
+  )
+}
+
+export default App
